87d3bdb7a1145fbf0c3629aef07248620869256b
milka_deluxe <milagalko1@gmail.com>
2021-03-22 19:51:05 +0200

    First OOP test
    Класс Numbers, который содержит два целых числа. Этот класс должен иметь две переменные-члены для хранения этих двух целых чисел. Вы также должны создать два метода

--- test_2/test_2/test_2/MyLib.hpp
+++ test_2/test_2/test_2/MyLib.hpp
@@ -2,3 +2,17 @@
 #include <stdio.h>
 
 using namespace std;
+
+class Numbers {
+public:
+    int n_1;
+    int n_2;
+    
+    void set(int n1, int n2) {
+        n_1 = n1;
+        n_2 = n2;
+    }
+    void print () {
+        cout << "Numbers (" << n_1 << "," << n_2 << ")" << endl;
+    }
+};
--- test_2/test_2/test_2/main.cpp
+++ test_2/test_2/test_2/main.cpp
@@ -1,5 +1,13 @@
 #include "MyLib.hpp"
 
 int main() {
+    
+    Numbers n1;
+    n1.set(3, 3);
+    
+    Numbers n2 { 4, 4};
+    
+    n1.print();
+    n2.print();
     return 0;
 }
